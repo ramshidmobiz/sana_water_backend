d110fc0a4ed4bec6e0bd39e061da7c79d2eee950
Ramshidali <aliramshid@gmail.com>
2024-03-08 10:37:52 +0530

    van model

--- van_management/models.py
+++ van_management/models.py
@@ -4,19 +4,19 @@ from master.models import *
 
 # Create your models here.
 class Van(models.Model):
-    van_id= models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    created_by = models.CharField(max_length=20,  blank=True)
-    created_date = models.DateTimeField(auto_now_add=True,blank=True, null=True)
+    van_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_by = models.CharField(max_length=20, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     modified_by = models.CharField(max_length=20, null=True, blank=True)
     modified_date = models.DateTimeField(blank=True, null=True)
     van_make = models.CharField(max_length=50)
     plate = models.CharField(max_length=50)
     renewal_date = models.DateTimeField(blank=True, null=True)
     insurance_expiry_date = models.DateTimeField(blank=True, null=True)
-    capacity = models.IntegerField(null=True,blank=True)
-    driver = models.ForeignKey('accounts.CustomUser', on_delete=models.SET_NULL, null=True, blank=True,related_name='driver_van',limit_choices_to={'user_type': 'Driver'})
-    salesman = models.ForeignKey('accounts.CustomUser', on_delete=models.SET_NULL, null=True, blank=True,related_name='salesman_van',limit_choices_to={'user_type': 'Salesman'})
-    branch_id = models.ForeignKey('master.BranchMaster', on_delete=models.SET_NULL, null=True, blank=True,related_name='van_branch',limit_choices_to={'user_type': 'Branch User'})
+    capacity = models.IntegerField(null=True, blank=True)
+    driver = models.ForeignKey('accounts.CustomUser', on_delete=models.SET_NULL, null=True, blank=True, related_name='driver_van', limit_choices_to={'user_type': 'Driver'})
+    salesman = models.ForeignKey('accounts.CustomUser', on_delete=models.SET_NULL, null=True, blank=True, related_name='salesman_van', limit_choices_to={'user_type': 'Salesman'})
+    branch_id = models.ForeignKey('master.BranchMaster', on_delete=models.SET_NULL, null=True, blank=True, related_name='van_branch', limit_choices_to={'user_type': 'Branch User'})
 
     class Meta:
         ordering = ('van_make',)
