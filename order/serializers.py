b95cf7bc3d87b01a154167bedfffc8c4587a308d
Ramshidali <aliramshid@gmail.com>
2024-04-08 15:15:46 +0530

    order

--- order/serializers.py
+++ order/serializers.py
@@ -1,20 +1,17 @@
 from rest_framework import serializers
-# from .models import Order_change, Order_return, Change_Reason
+from .models import Order_change, Order_return, Change_Reason
 
 class OrderChangeSerializer(serializers.ModelSerializer):
-    pass
-    # class Meta:
-    #     model = Order_change
-    #     fields = '__all__'
+    class Meta:
+        model = Order_change
+        fields = '__all__'
 
 class OrderReturnSerializer(serializers.ModelSerializer):
-    pass
-    # class Meta:
-    #     model = Order_return
-    #     fields = '__all__'
+    class Meta:
+        model = Order_return
+        fields = '__all__'
 
 class ChangeReasonSerializer(serializers.ModelSerializer):
-    pass
-    # class Meta:
-    #     model = Change_Reason
-    #     fields = '__all__'
+    class Meta:
+        model = Change_Reason
+        fields = '__all__'
